937fa7f4483947330f8e9c190258bac71c9a9776
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-08-01 18:16:53 +0300

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const listOfLikeButtons = document.querySelectorAll(".like span");
+const modal = document.getElementsByClassName("hidden")[0];
 
-
-
+for(const likeButton of listOfLikeButtons){
+  likeButton.addEventListener("click", (e)=>{
+    mimicServerCall()
+    .then( ()=>{ if(likeButton.innerHTML === EMPTY_HEART){
+      likeButton.innerHTML = FULL_HEART
+      likeButton.classList.add("activated-heart")
+    }
+    else{
+      likeButton.innerHTML = EMPTY_HEART
+      likeButton.classList.remove("activated-heart")
+    }
+  })
+  .catch((error) => {
+    modal.classList.remove("hidden")
+    setTimeout(()=>{
+      modal.classList.add("hidden")
+    },5000)
+  })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
@@ -23,3 +43,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
